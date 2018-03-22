5fc5b0377d347abdd5d6f16b617cd330d6dd4e14
thomasshouler <thomas.shouler@gmail.com>
2018-03-22 18:23:04 -0500

    Upgrade to junit 5

--- source-app/src/test/java/thomastodon/io/sourceapp/SourceAppApplicationTests.java
+++ source-app/src/test/java/thomastodon/io/sourceapp/SourceAppApplicationTests.java
@@ -1,16 +1,16 @@
 package thomastodon.io.sourceapp;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
-public class SourceAppApplicationTests {
+@ExtendWith(SpringExtension.class)
+class SourceAppApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	void contextLoads() {
 	}
 
 }
