9997b1eeb7ef645bd6760abb114a15c2c59bad42
thomasshouler <thomas.shouler@gmail.com>
2018-03-22 18:01:15 -0500

    Initialize a source

---
+++ source-app/src/main/java/thomastodon/io/sourceapp/SourceAppApplication.java
@@ -0,0 +1,12 @@
+package thomastodon.io.sourceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SourceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SourceAppApplication.class, args);
+	}
+}
