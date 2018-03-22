cc4b0dcac4df2db67cca0832f25f1c4647b82236
thomasshouler <thomas.shouler@gmail.com>
2018-03-22 18:29:01 -0500

    Initialize a sink app

---
+++ sink-app/src/main/java/thomastodon/io/sinkapp/SinkAppApplication.java
@@ -0,0 +1,12 @@
+package thomastodon.io.sinkapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinkAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SinkAppApplication.class, args);
+	}
+}
---
+++ sink-app/src/test/java/thomastodon/io/sinkapp/SinkAppApplicationTests.java
@@ -0,0 +1,16 @@
+package thomastodon.io.sinkapp;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+class SinkAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
