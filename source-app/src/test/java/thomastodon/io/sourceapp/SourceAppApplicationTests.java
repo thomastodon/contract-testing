a98074e6e1076ffb369ad5b648db93e2f9c6e4f3
thomasshouler <thomas.shouler@gmail.com>
2018-06-27 22:20:56 -0500

    Test that we integrate with rabbitmq

--- source-app/src/test/java/thomastodon/io/sourceapp/SourceAppApplicationTests.java
+++ source-app/src/test/java/thomastodon/io/sourceapp/SourceAppApplicationTests.java
@@ -1,16 +1,34 @@
 package thomastodon.io.sourceapp;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
 class SourceAppApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    @DisplayName("the app puts something on the queue")
+    void contextLoads() throws IOException {
 
+        CachingConnectionFactory cachingConnectionFactory = new CachingConnectionFactory("localhost", 5672);
+        cachingConnectionFactory.setUsername("guest");
+        cachingConnectionFactory.setPassword("guest");
+
+        int messageCount = cachingConnectionFactory
+            .createConnection()
+            .createChannel(false)
+            .basicGet("egg-queue", false)
+            .getMessageCount();
+
+        assertThat(messageCount).isGreaterThan(0);
+    }
 }
